b3696d30609bdff9bab638bfc63b9a6077257435
Qdzia <46885229+Qdzia@users.noreply.github.com>
2021-02-23 14:58:08 +0100

    Static work and contact form

--- src/elements/contact.js
+++ src/elements/contact.js
@@ -1,13 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/elements.css";
+import {Form, Row, Col, Button} from "react-bootstrap"
 
 const contact = () => {
   return (
-    <div class="bg-dark text-white ">
-      <h1 class="display-1 d-flex justify-content-center text-center">
-        <strong>contact</strong>
-      </h1>
-      <br />
-      <p class="lead d-flex justify-content-center text-center">contact</p>
+    <div class="bg-dark text-white element-container">
+      <div class="w-auto">
+        <h3 class="text-light mb-4">Contact </h3>
+      </div>
+
+      <Form>
+  <Row>
+    <Col>
+    <Form.Label>Name</Form.Label>
+      <Form.Control placeholder="First name" />
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Label>Subject</Form.Label>
+      <Form.Control placeholder="Your Subject" />
+    </Col>
+    <Col>
+    <Form.Label>Massage</Form.Label>
+      <Form.Control as="textarea" rows={3} />
+
+    </Col>
+  </Row>
+  <Button variant="primary" type="submit" >
+    Submit
+  </Button>
+</Form>
     </div>
   );
 };
--- src/elements/projects.js
+++ src/elements/projects.js
@@ -1,13 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Pagination from "react-bootstrap/Pagination";
 
 const projests = () => {
   return (
-    <div class="bg-dark text-white ">
-      <h1 class="display-1 d-flex justify-content-center text-center">
-        <strong>projests</strong>
-      </h1>
-      <br />
-      <p class="lead d-flex justify-content-center text-center">projests</p>
+    <div class="bg-dark text-dark element-container">
+      <div class="w-auto">
+        <h3 class="text-light mb-4">Work </h3>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">Monopoly the game</h4>
+          <h6 class="card-subtitle mb-2 text-muted">
+            Group • March 2010 - Present | React • JSX • Redux • ASP .NET
+          </h6>
+
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.orum et dolorum fuga. Et harum quidem
+            rerum facilis est et expedita distinctio. Nam libero tempore, cum
+            soluta nobis est eligendi optio cumque nihil impedit quo minus id
+            quod maxime placeat facere possimus, omnis voluptas assumenda est,
+            omnis dolor repellendus. Temporibus autem quibusdam et aut officiis
+            debitis aut rerum
+          </p>
+          <a href="#" class="card-link">
+            Github
+          </a>
+        </div>
+      </div>
+      <p>
+        <br />
+      </p>
+      <div class="d-flex justify-content-center">
+        <Pagination>
+          <Pagination.Item>{1}</Pagination.Item>
+          <Pagination.Item>{2}</Pagination.Item>
+          <Pagination.Item>{3}</Pagination.Item>
+          <Pagination.Item>{4}</Pagination.Item>
+        </Pagination>
+      </div>
     </div>
   );
 };
